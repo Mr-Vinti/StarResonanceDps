466a828eeac30000f450a56a4913100d814396d3
Rocy-June <1048654629@qq.com>
2025-10-19 00:23:14 +0900

    Refactor: Refactor SwitchControl to support i18n

--- StarResonanceDpsAnalysis.WPF/Converters/HalfValueConverter.cs
+++ StarResonanceDpsAnalysis.WPF/Converters/HalfValueConverter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
@@ -6,10 +7,37 @@ namespace StarResonanceDpsAnalysis.WPF.Converters;
 
 public class HalfValueConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is double d) return new CornerRadius(d / 2);
-        return new CornerRadius(0);
+        if (!ConverterNumberHelper.TryToDouble(value, out var original))
+        {
+            original = 0d;
+        }
+
+        var half = original / 2d;
+
+        if (targetType == typeof(double) || targetType == typeof(double?))
+        {
+            return half;
+        }
+
+        if (targetType == typeof(float) || targetType == typeof(float?))
+        {
+            return (float)half;
+        }
+
+        if (targetType == typeof(decimal) || targetType == typeof(decimal?))
+        {
+            return (decimal)half;
+        }
+
+        if (targetType == typeof(string))
+        {
+            return half.ToString(culture);
+        }
+
+        // Default to CornerRadius for compatibility with existing usages.
+        return new CornerRadius(half);
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
